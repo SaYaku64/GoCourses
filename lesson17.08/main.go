cc7ee6c75987bc98c5fff542832b60f66a312eec
SaYaku64 <yakushev6400@gmail.com>
2020-08-16 18:40:13 +0300

    Added comments and fixed some bugs

--- lesson17.08/main.go
+++ lesson17.08/main.go
@@ -12,26 +12,35 @@ import (
 	"strconv"
 )
 
+var printed bool
+
 func main() {
 	fmt.Println("[Server started]")
 
+	// map of employees
 	var emp = make(map[int]employee)
 	emp[1] = employee{"Alexander Yakushev", 20, 3}
 	emp[2] = employee{"Tony Montana", 40, 15}
 	emp[3] = employee{"abc", 1, 1}
 
+	// start page, that uses html-file (localhost/)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "user.html")
 	})
 
+	// handle for get query
 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+
 		fmt.Fprintf(w, "Hello Master!\n")
+
 		id := ""
 		query := r.URL.Query()
 		id = query.Get("id")
+
 		if id == "" {
 			fmt.Fprintf(w, "You haven't printed the ID\n")
 		} else {
+			// checking if ID is a number
 			intID, err := strconv.Atoi(id)
 			if err != nil {
 				fmt.Println(err)
@@ -44,27 +53,30 @@ func main() {
 	})
 
 	http.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request) {
+		// getting values from post query
 		name := r.FormValue("username")
 		age := r.FormValue("userage")
 		exp := r.FormValue("userexp")
 
+		fmt.Fprintf(w, "Entered   ||   Full name: %s Age: %s Experience: %s\n", name, age, exp)
+
+		// making int versions of age and exp for manipulations
 		intAge, err1 := strconv.Atoi(age)
 		intExp, err2 := strconv.Atoi(exp)
 		if err1 != nil || err2 != nil {
 			fmt.Println(err1)
 			fmt.Println(err2)
 			fmt.Fprintf(w, "Something wrong with age or exp!\n")
-		}
-
-		fmt.Fprintf(w, "Entered   ||   Full name: %s Age: %s Experience: %s\n", name, age, exp)
-		for i := 0; i < len(emp); i++ {
-			if emp[i].fullName == name && emp[i].age == intAge && emp[i].experience == intExp {
-				fmt.Fprintf(w, "ID: %v\n", i)
-			} else if i == len(emp)-1 {
-				fmt.Fprintf(w, "No matches! Check mistakes!\n")
+		} else {
+			for i := 1; i < len(emp)+1; i++ {
+				if emp[i].fullName == name && emp[i].age == intAge && emp[i].experience == intExp {
+					fmt.Fprintf(w, "ID: %v\n", i)
+					printed = true
+				} else if i == len(emp) && printed == false {
+					fmt.Fprintf(w, "No matches! Check mistakes!\n")
+				}
 			}
 		}
-		fmt.Println(name, age, exp)
 	})
 
 	http.ListenAndServe(":80", nil)
